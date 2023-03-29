af0bcdb8e1aa83e42ad73d93dcea0872bc7de23e
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-03-29 15:50:32 -0700

    implemented basic code for card flipping game, need to add parameters to meet extra requirements

--- memory-game.js
+++ memory-game.js
@@ -1,30 +1,19 @@
 "use strict";
 
-/** Memory game: find matching pairs of cards and flip both of them. */
-
 const FOUND_MATCH_WAIT_MSECS = 1000;
 const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
+  "red", "blue", "green", "orange", "purple", "grey", "yellow", "brown",
+  "red", "blue", "green", "orange", "purple", "grey", "yellow", "brown"
 ];
 
 const colors = shuffle(COLORS);
 
 createCards(colors);
 
-
-/** Shuffle array items in-place and return shuffled array. */
-
 function shuffle(items) {
-  // This algorithm does a "perfect shuffle", where there won't be any
-  // statistical bias in the shuffle (many naive attempts to shuffle end up not
-  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
-  // you're interested, you can learn about it, but it's not important.
 
   for (let i = items.length - 1; i > 0; i--) {
-    // generate a random index between 0 and i
     let j = Math.floor(Math.random() * i);
-    // swap item at i <-> item at j
     [items[i], items[j]] = [items[j], items[i]];
   }
 
@@ -42,24 +31,84 @@ function createCards(colors) {
   const gameBoard = document.getElementById("game");
 
   for (let color of colors) {
-    // missing code here ...
+
+    let card = document.createElement('div');
+    card.classList.add(color);
+    gameBoard.appendChild(card);
+    card.addEventListener('click', handleCardClick);
   }
 }
+
+
+// set variables
+let firstCard;
+let secondCard;
+let currentTurn = false;
+let score = 0;
 
 /** Flip a card face-up. */
 
 function flipCard(card) {
-  // ... you need to write this ...
+  card.style.backgroundColor = `${card.classList[0]}`;
 }
 
 /** Flip a card face-down. */
 
 function unFlipCard(card) {
-  // ... you need to write this ...
+  card.style.backgroundColor = '';
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
 
-function handleCardClick(evt) {
-  // ... you need to write this ...
+function handleCardClick(e) {
+  let current = e.target;
+
+  if (currentTurn) {
+    return;
+  }
+
+  if (current.classList.contains('correct')) {
+    return;
+  }
+
+  flipCard(current);
+
+  console.log(current.className);
+
+  if (firstCard === undefined) {
+    firstCard = current;
+    score++;
+  } else if (secondCard === undefined) {
+    secondCard = current;
+    score++;
+  }
+  if (firstCard && secondCard){
+    if ((firstCard.className) === (secondCard.className)) {
+      firstCard.setAttribute('class', 'correct');
+      secondCard.setAttribute('class', 'correct');
+      firstCard.removeEventListener('click', handleCardClick);
+      secondCard.removeEventListener('click', handleCardClick);
+      firstCard = undefined;
+      secondCard = undefined;
+      currentTurn = false;
+    } else if (firstCard.className !== secondCard.className) {
+      setTimeout(function() {
+        unFlipCard(firstCard);
+        unFlipCard(secondCard);
+        firstCard = undefined;
+        secondCard = undefined;
+        currentTurn = false;
+      }, 1000);
+    }
+  }
 }
+
+
+/* PSUEDO-CODE */
+/*
+when clicking a card, invoke the card click function
+change the card to the color of the background
+
+
+
+*/
